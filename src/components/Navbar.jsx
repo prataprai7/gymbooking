20c7675e3b47a924fad689fac4e48e10373d15b6
pratap <pratapbantawa3@gmail.com>
2025-06-09 08:13:03 +0545

    gyhb

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -41,7 +41,7 @@ const Navbar = () => {
 
                 {/* Logo */}
                 <Link to='/'>
-                    <img src={assets.bb} alt="logo" className={`h-9 ${isScrolled && "invert opacity-80"}`} />
+                    <img src={assets.bayam} alt="logo" className={`h-9 ${isScrolled && "invert opacity-80"}`} />
                 </Link>
 
                 {/* Desktop Nav */}
